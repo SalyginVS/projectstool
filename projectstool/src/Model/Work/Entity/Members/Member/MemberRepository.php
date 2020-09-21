4b7e606bb9b190ba80570642e92b1bb846efefa8
SalyginVS <salyginvs@gmail.com>
2020-09-21 14:05:41 +0300

    Added group members tabs

--- projectstool/src/Model/Work/Entity/Members/Member/MemberRepository.php
+++ projectstool/src/Model/Work/Entity/Members/Member/MemberRepository.php
@@ -6,6 +6,7 @@ namespace App\Model\Work\Entity\Members\Member;
 
 use App\Model\EntityNotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Model\Work\Entity\Members\Group\Id as GroupId;
 
 class MemberRepository
 {
@@ -27,6 +28,15 @@ class MemberRepository
                 ->select('COUNT(t.id)')
                 ->andWhere('t.id = :id')
                 ->setParameter(':id', $id->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function hasByGroup(GroupId $id): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.group = :group')
+                ->setParameter(':group', $id->getValue())
                 ->getQuery()->getSingleScalarResult() > 0;
     }
 
