7f9d002f610bcd2f987a78a74b7e462aab05f9aa
SalyginVS <salyginvs@gmail.com>
2020-09-21 12:49:23 +0300

    Added user member & create mamber link

---
+++ projectstool/src/ReadModel/Work/Members/Member/Filter/Filter.php
@@ -0,0 +1,15 @@
+<?php
+declare(script_types=1);
+
+
+namespace App\ReadModel\Work\Members\Member\Filter;
+
+use App\Model\Work\Entity\Members\Member\Status;
+
+class Filter
+{
+    public $name;
+    public $email;
+    public $group;
+    public $status = Status::ACTIVE;
+}
