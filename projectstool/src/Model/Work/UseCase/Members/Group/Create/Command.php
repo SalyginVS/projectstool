8b1194ae858b8c5616b0c937e837ec20f37622c7
SalyginVS <salyginvs@gmail.com>
2020-09-21 11:03:51 +0300

    Added work member groups

---
+++ projectstool/src/Model/Work/UseCase/Members/Group/Create/Command.php
@@ -0,0 +1,16 @@
+<?php
+declare(script_types=1);
+
+
+namespace App\Model\Work\UseCase\Members\Group\Create;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    public $name;
+}
